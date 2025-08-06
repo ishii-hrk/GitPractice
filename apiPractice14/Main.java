d777936881fabc5056f7a6824638362193687786
藤原 雅斗 <pc3@plusv72124.onmicrosoft.com>
2025-08-06 19:03:08 +0900

    committed

---
+++ apiPractice14/Main.java
@@ -0,0 +1,15 @@
+package apiPractice14;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BankAccount a = new BankAccount();
+		a.accountNumber = "4649";
+		a.balance = 1592;
+		
+		System.out.println(a);
+		
+
+	}
+
+}
